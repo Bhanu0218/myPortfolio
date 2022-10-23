fe02f75253a8b7dfce892748f5aab4264c9900b6
Bhanu0218 <91757901+Bhanu0218@users.noreply.github.com>
2022-10-23 15:03:41 +0530

    Added files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+// Typing Animation
+
+var typed = new Typed(".typing", {
+    strings: ["Engineer", "Devops-Engineer", "Web-Developer", "Android App Developer", "Big-data Engineer"],
+    typeSpeed: 100,
+    backSpeed: 60,
+    loop: true
+})
+var typed = new Typed(".typing-2", {
+    strings: ["Engineer", "Devops-Engineer", "Web-Developer", "Android App Developer", "Big-data Engineer"],
+    typeSpeed: 99,
+    backSpeed: 58,
+    loop: true
+})
